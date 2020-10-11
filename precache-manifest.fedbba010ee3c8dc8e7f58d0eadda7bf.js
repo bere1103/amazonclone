5949e85eb26f2ea7b9844ce2b839ca07d7645040
bere1103 <64001151+bere1103@users.noreply.github.com>
2020-10-11 22:51:19 +0800

    Add files via upload
    amazonclone

---
+++ precache-manifest.fedbba010ee3c8dc8e7f58d0eadda7bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f98b362ef530a656da00fac7096f6e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8066a9788c1f09ac9d46",
+    "url": "/static/css/main.78c7c615.chunk.css"
+  },
+  {
+    "revision": "66057293e9aff8f36eaf",
+    "url": "/static/js/2.1152b9cd.chunk.js"
+  },
+  {
+    "revision": "2b587c35db5df3608932eb682dfa83dc",
+    "url": "/static/js/2.1152b9cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8066a9788c1f09ac9d46",
+    "url": "/static/js/main.3bbf00f0.chunk.js"
+  },
+  {
+    "revision": "92a146e5ab9e0f9e785b",
+    "url": "/static/js/runtime-main.90140090.js"
+  }
+]);
